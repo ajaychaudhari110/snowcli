6ab16afebdbec4d491caf23da2f5dfa5e9efc395
ajaychaudhari110 <43629795+ajaychaudhari110@users.noreply.github.com>
2024-04-04 14:06:56 +0530

    Update cdp_create_warehouse_standard.sql

--- cdp_create_warehouse_standard.sql
+++ cdp_create_warehouse_standard.sql
@@ -20,6 +20,25 @@ declare
     var_wh_description varchar;
     var_global_deployment_id varchar;
 begin
+    SELECT GETVARIABLE('OR_REPLACE') INTO var_or_replace;
+    SELECT GETVARIABLE('IF_NOT_EXISTS') INTO var_if_not_exists;
+    SELECT GETVARIABLE('WH_NAME') INTO var_wh_name;
+    SELECT GETVARIABLE('WH_SIZE') INTO var_wh_size;
+    SELECT GETVARIABLE('MAX_CLUST_SIZE') INTO var_max_clust_size;
+    SELECT GETVARIABLE('MIN_CLUST_SIZE') INTO var_min_clust_size;
+    SELECT GETVARIABLE('set SCALING_POLICY') INTO var_scaling_policy;
+    SELECT GETVARIABLE('AUTOSUSPEND_SECS') INTO var_autosuspend_secs;
+    SELECT GETVARIABLE('WAREHOUSE_TYPE') INTO var_warehouse_type;
+    SELECT GETVARIABLE('GLOBAL_PRIMARY_OWNER') INTO var_global_primary_owner;
+    SELECT GETVARIABLE('GLOBAL_SECONDARY_OWNER') INTO var_global_secondary_owner;
+    SELECT GETVARIABLE('GLOBAL_OWNER_GROUP') INTO var_global_owner_group;
+    SELECT GETVARIABLE('GLOBAL_BUSINESS_OWNER') INTO var_global_business_owner;
+    SELECT GETVARIABLE('GLOBAL_BUSINESS_UNIT') INTO var_global_business_unit;
+    SELECT GETVARIABLE('GLOBAL_BUSINESS_SUB_UNIT') INTO var_global_business_sub_unit;
+    SELECT GETVARIABLE('GLOBAL_BUC') INTO var_global_buc;
+    SELECT GETVARIABLE('WH_DESCRIPTION') INTO var_wh_description;
+    SELECT GETVARIABLE('GLOBAL_DEPLOYMENT_ID') INTO var_global_deployment_id;        
+    
     sql := 'CREATE ' || var_or_replace || 'WAREHOUSE ' || var_if_not_exists || ' ' || var_wh_name;
     sql := sql || ' WITH';
     sql := sql || ' WAREHOUSE_SIZE=' || var_wh_size;
