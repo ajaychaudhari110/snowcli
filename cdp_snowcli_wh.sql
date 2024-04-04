bab572bf62a1e0ba534c4f3c520c95c19aed96e3
ajaychaudhari110 <43629795+ajaychaudhari110@users.noreply.github.com>
2024-04-04 13:55:32 +0530

    Update cdp_snowcli_wh.sql

--- cdp_snowcli_wh.sql
+++ cdp_snowcli_wh.sql
@@ -35,6 +35,25 @@ begin
     var_wh_description :='Short description';
     var_global_deployment_id :='12345';
 
+    set OR_REPLACE = :var_or_replace;
+    set IF_NOT_EXISTS = :var_if_not_exists;
+    set WH_NAME = :var_wh_name;
+    set WH_SIZE = :var_wh_size;
+    set MAX_CLUST_SIZE = :var_max_clust_size;
+    set MIN_CLUST_SIZE = :var_min_clust_size;
+    set SCALING_POLICY = :var_scaling_policy;
+    set AUTOSUSPEND_SECS = :var_autosuspend_secs;
+    set WAREHOUSE_TYPE = :var_warehouse_type;
+    set GLOBAL_PRIMARY_OWNER = :var_global_primary_owner;
+    set GLOBAL_SECONDARY_OWNER = :var_global_secondary_owner;
+    set GLOBAL_OWNER_GROUP = :var_global_owner_group;
+    set GLOBAL_BUSINESS_OWNER = :var_global_business_owner;
+    set GLOBAL_BUSINESS_UNIT = :var_global_business_unit;
+    set GLOBAL_BUSINESS_SUB_UNIT = :var_global_business_sub_unit;
+    set GLOBAL_BUC = :var_global_buc;
+    set WH_DESCRIPTION = :var_wh_description;
+    set GLOBAL_DEPLOYMENT_ID = :var_global_deployment_id;
+
     EXECUTE IMMEDIATE FROM @myco_db.integrations.snowflake_cli/branches/main/cdp_create_warehouse_standard.sql;
     
 end;
