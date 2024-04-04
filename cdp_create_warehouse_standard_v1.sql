d00bf0cdab001f2b75b9dc5e701c8cadea9e809b
ajaychaudhari110 <43629795+ajaychaudhari110@users.noreply.github.com>
2024-04-04 15:40:50 +0530

    Update cdp_create_warehouse_standard_v1.sql

--- cdp_create_warehouse_standard_v1.sql
+++ cdp_create_warehouse_standard_v1.sql
@@ -20,7 +20,7 @@ declare
     var_wh_description varchar;
     var_global_deployment_id varchar;
 begin
-    sql := 'CREATE ' || var_or_replace || 'WAREHOUSE ' || var_if_not_exists || ' ' || var_wh_name || ' WITH' || ' WAREHOUSE_SIZE=' || var_wh_size || ' MAX_CLUSTER_COUNT=' || var_max_clust_size  || ' MIN_CLUSTER_COUNT=' || var_min_clust_size || ' SCALING_POLICY=' || var_scaling_policy  || ' AUTO_SUSPEND=' || var_autosuspend_secs || ' WAREHOUSE_TYPE=' || var_warehouse_type || ' AUTO_RESUME= TRUE INITIALLY_SUSPENDED = TRUE' || ' COMMENT=\'{ ' || '"primary_owner" : "' || var_global_primary_owner || '", ' || '"secondary_owner" : "' || var_global_secondary_owner || '", ' || '"owner_group" : "' || var_global_owner_group || '", ' || '"business_owner" : "' || var_global_business_owner || '", ' || '"business_unit" : "' || var_global_business_unit || '", ' || '"business_sub_unit" : "' || var_global_business_sub_unit || '", ' || '"buc" : "' || var_global_buc || '", ' || '"description" : "' || var_wh_description || '", ' || '"deployment_id" : "' || var_global_deployment_id || '" ' || '}\';';
+    sql := 'CREATE ' || var_or_replace || 'WAREHOUSE ' || var_if_not_exists || ' ' || var_wh_name;
     select :sql;
 end;
 $$
